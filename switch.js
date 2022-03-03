942d9353e24984d5e0b6cf075e427c789a133f83
sagarpanchani <sagarkmar92@gmail.com>
2022-03-03 23:01:37 +0500

    Switch case

---
+++ switch.js
@@ -0,0 +1,62 @@
+
+
+// SWITCH STATSEMENT
+
+var days = prompt("Monday")
+
+switch(days){
+    case "Monday": console.log("today is monday")
+    break;
+    case "Tuesday": console.log("today is Tuesday")
+    break;  
+     case "Wedesnday": console.log("today is Wedesnday")
+    break; 
+      case "Thursday": console.log("today is Thursday")
+    break;
+    case "Firday": alert("today is OffDay")
+    break;
+    case "Saturaday": console.log("today is Saturaday")
+    break;
+    case "Sunday": console.log("today is Sunday")
+    break;
+    default: "Enter your correct day"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var i = 0;
+do{
+    i++
+    console.log(i)
+  
+}while(i < 9)
+
+
+
